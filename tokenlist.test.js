252f5a028be31788102b7e632ebbb3b65072bb58
murloc <81496776+0xmurloc@users.noreply.github.com>
2023-07-20 09:02:51 +0200

    first commit

--- tokenlist.test.js
+++
@@ -1,7 +0,0 @@
-describe("Token list should be a valid JSON", () => {
-  it("can parse", () => {
-    expect(
-      JSON.parse(require("fs").readFileSync("./mc.tokenlist.json", "utf8"))
-    ).toBeTruthy();
-  });
-});
